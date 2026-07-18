677e8ccb9b0eb726c3531d75e293625526d7e0e6
Chaudharyjayant <chaudharyjayant980@gmail.com>
2026-07-18 19:08:09 +0530

    Added the all paths to the list

--- Recursion/Maze-Problems/Question2.java
+++ Recursion/Maze-Problems/Question2.java
@@ -1,22 +1,28 @@
-//Same as last question just need to print all the ways
+
+import java.util.ArrayList;
+
+//Same as last question just need to print all the ways or paths or we could say add them to the list
 
 public class Question2 {
     public static void main(String[] args) {
-        path("", 3, 3);
+        ArrayList<String> list = new ArrayList<>();
+        path("", 3, 3,list);
+
+        System.out.println(list);
     }
 
-    static void path(String p,int r,int c){
+    static void path(String p,int r,int c,ArrayList<String> list){
         if(r==1 && c==1){
-            System.out.println(p);
+            list.add(p);
             return;
         }
 
         if( r > 1) {
-            path(p + 'D', r-1, c);
+            path(p + 'D', r-1, c,list);
         }
 
         if(c>1){
-            path(p+'R', r, c-1);
+            path(p+'R', r, c-1,list);
         }
     }
 }
