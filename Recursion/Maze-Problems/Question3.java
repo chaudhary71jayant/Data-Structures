edd76588455d2e4493cd0f33b496be75c30c2461
Chaudharyjayant <chaudharyjayant980@gmail.com>
2026-07-18 19:08:51 +0530

    Approach to reach the bottom when moving diagonally is also allowed along with moving right and down

---
+++ Recursion/Maze-Problems/Question3.java
@@ -0,0 +1,22 @@
+/*
+    The maze problem where we can move right down and diagnolly as well
+ */
+
+public class Question3 {
+    public static void main(String[] args) {
+        System.out.println(numberOfWays(2,3));
+    }
+
+    static int numberOfWays(int r,int c){
+        //base condtion
+        if(r == 1 && c ==1) return 1;
+        
+        int ways = 0;
+
+        if(r > 1) ways += numberOfWays(r-1, c);
+        if(c > 1) ways += numberOfWays(r, c-1);
+        if(r>1 && c >1) ways += numberOfWays(r-1, c-1);
+
+        return ways;
+    }
+}
